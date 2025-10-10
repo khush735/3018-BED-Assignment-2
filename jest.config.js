9dafb6ca048879c92a54924c046473e6a7bdbe4e
khush735 <khushpatel732005@gmail.com>
2025-10-10 15:56:30 -0500

    chore: Update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/test/**/*.test.ts']
+  testMatch: ['**/test/**/*.test.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts']
 };
