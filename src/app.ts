f51e30ed9d52a899dd83569dfdb101c30e83083a
khush735 <khushpatel732005@gmail.com>
2025-11-18 15:15:41 -0600

    Update: update app.ts with envirnoment and helmetconfig

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-// app.ts
 import express from 'express';
+import dotenv from 'dotenv';
+import { getHelmetConfig } from "../config/helmetConfig";
 import branchRoutes from './api/v1/routes/branchRoutes';
 import employeeRoutes from './api/v1/routes/employeeRoutes';
+
+// Load environment variables first
+dotenv.config();
+
+// Apply custom Helmet configuration
+app.use(getHelmetConfig());
 
 const app = express();
 app.use(express.json());
